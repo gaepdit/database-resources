3bdbc9cd5cd49179ad87fd5f6755caa0a424f095
Doug Waldron <douglas.waldron@dnr.ga.gov>
2018-03-01 07:25:36 -0500

    Update templates

--- Templates/Stored procedure template.sql
+++ Templates/Stored procedure template.sql
@@ -5,15 +5,15 @@ GO
 SET QUOTED_IDENTIFIER ON;
 GO
 
-IF OBJECT_ID('dbo.MyProcedure') IS NOT NULL
-    DROP PROCEDURE dbo.MyProcedure;
+IF OBJECT_ID('dbo.MyProcedure') IS NULL
+    EXEC ('CREATE PROCEDURE dbo.MyProcedure AS SET NOCOUNT ON;')
 GO
 
 /*****************************************************************************
 
-Author:     Brian Gregory
-Created:    2017-??-??
-Overview:   General statement about WHAT the procedure is doing
+Author:     Doug Waldron
+Created:    2018-??-??
+Purpose:    General statement about WHAT the procedure is doing
 
 Input Parameters:
     @inputParameter1 - Description
@@ -22,42 +22,46 @@ Input Parameters:
 Output Parameters:
     None
 
+Returns:
+     0 on success
+    -1 on error
+
 Tables written to:
     MyTable1
-  
+
 Tables accessed:
     MyTable2
 
 Modification History:
 When        Who                 What
 ----------  ------------------  ----------------------------------------
-2017-??-??  BGregory            Initial Version
+2018-??-??  DWaldron            Initial Version
 
 *******************************************************************************/
 
-CREATE PROCEDURE dbo.MyProcedure
-       @inputParameter1 varchar(1)
-     , @inputParameter2 int
+ALTER PROCEDURE dbo.MyProcedure
+          @inputParameter1 VARCHAR(1)
+        , @inputParameter2 INT
 AS
-     SET XACT_ABORT, NOCOUNT ON;
+    SET XACT_ABORT, NOCOUNT ON;
     BEGIN TRY
-        -- Declare variables here if needed
+    -- Declare variables here if needed
 
-        BEGIN TRANSACTION;
+    BEGIN TRANSACTION;
 
-        -- Add commands here
-        -- Can include multiple transactions if needed
+    -- Add commands here
+    -- Can include multiple transactions if needed
 
-        COMMIT TRANSACTION;
+    COMMIT TRANSACTION;
 
-        -- Add final command here to select return data or assign output parameter values if needed 
+    -- Add final command here to select return data or assign output parameter values if needed
     END TRY
     BEGIN CATCH
-        IF @@trancount > 0
-            ROLLBACK TRANSACTION;
-        DECLARE @msg nvarchar(2048) = ERROR_MESSAGE();
-        RAISERROR(@msg, 16, 1);
-        RETURN -1;
+    IF @@trancount > 0
+        ROLLBACK TRANSACTION;
+    DECLARE @msg NVARCHAR(2048) = ERROR_MESSAGE();
+    RAISERROR (@msg, 16, 1);
+    RETURN -1;
     END CATCH;
 
 GO
